f8ff2efc0f7021b0f03e55b22e736f8439116b44
赵学官 <2214114760@qq.com>
2021-10-01 21:23:54 +0800

    玩家基本移动（赵学官）
    实现了水平移动，跑步动画切换，跳跃

---
+++ HBQ_GamJim/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("speed",Mathf.Abs(rb.velocity.x));   
+    }
+}
---
+++ HBQ_GamJim/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+    public float jumpForce;
+
+    
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask groundLayer;
+
+    [Header("States Check")]
+    public bool isGround;
+    public bool canJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        PhysicsCheck();
+        Movement();
+        Jump();
+    }
+    void CheckInput()
+    {
+        if(Input.GetButtonDown("Jump")&&isGround)
+        {
+            canJump = true;
+        }
+    }
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        if(horizontalInput!=0)
+        {
+            transform.localScale = new Vector3(-horizontalInput, 1, 1);
+        }
+    }
+
+    void Jump()
+    {
+        if(canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.gravityScale = 4;
+            canJump = false;
+        }
+    }
+
+    void PhysicsCheck()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position,checkRadius,groundLayer);
+        if(isGround)
+        {
+            
+            rb.gravityScale = 1;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+    }
+}
